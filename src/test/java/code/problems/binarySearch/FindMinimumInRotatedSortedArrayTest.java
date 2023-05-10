3649d491b62b70c7f8c9ce4b51f83231759368af
levi <saad.dr98@gmail.com>
2023-05-10 23:49:01 +0500

    binary search: find minimum in rotated sorted array (test case edit)

--- src/test/java/code/problems/binarySearch/FindMinimumInRotatedSortedArrayTest.java
+++ src/test/java/code/problems/binarySearch/FindMinimumInRotatedSortedArrayTest.java
@@ -16,6 +16,7 @@ class FindMinimumInRotatedSortedArrayTest {
             assertEquals(1, FindMinimumInRotatedSortedArray.find(List.of(3,4,5,1,2)));
             assertEquals(0, FindMinimumInRotatedSortedArray.find(List.of(4,5,6,7,0,1,2)));
             assertEquals(11, FindMinimumInRotatedSortedArray.find(List.of(11,13,15,17)));
+            assertEquals(0, FindMinimumInRotatedSortedArray.find(List.of(0)));
         });
     }
 }
