deefe3b3c8f0e4b2aa0c815ac6d5ec249f593b1e
levi <saad.dr98@gmail.com>
2023-05-13 23:20:51 +0500

    setup for Linked List questions

---
+++ src/main/java/code/problems/linkedlists/ListNode.java
@@ -0,0 +1,46 @@
+package code.problems.linkedlists;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListNode {
+
+    public Integer value;
+    public ListNode next;
+
+    public ListNode(Integer value, ListNode next){
+        this.value = value;
+        this.next = next;
+    }
+
+    public static List<Integer> toArray(ListNode head){
+        List<Integer> arr = new ArrayList<>();
+
+        while (head != null){
+            arr.add(head.value);
+            head = head.next;
+        }
+
+        return arr;
+    }
+
+    public static ListNode toLinkedList(List<Integer> nums){
+        if(nums.size() == 0){
+            return null;
+        }
+
+        ListNode prev = null;
+        ListNode curr = new ListNode(nums.get(0), null);
+
+        ListNode head = curr;
+
+        for(int i = 1; i < nums.size(); i++){
+            prev = curr;
+            curr.next = new ListNode(nums.get(i), null);
+            curr = curr.next;
+        }
+
+        return head;
+    }
+}
